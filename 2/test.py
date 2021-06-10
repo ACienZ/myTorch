38da5eafe284f27b0d2b9794fb613e476a5ed746
ACienZ <xijian.rui@fuji.waseda.jp>
2021-06-10 12:53:53 +0900

    update lesson2

--- 2/test.py
+++ 2/test.py
@@ -1,24 +1,94 @@
-# C=3
-# idx=500
-# # print(list(range(0,-1)))
-# L=[]
-# if idx==0:
-#     L=[1,2,3]
-# elif idx==1:
-#     L=[0,2,3,4]
-# elif idx ==2:
-#     L=[0,1,3,4,5]
-# else:
-#     L=list(range(idx-C,idx))+list(range(idx+1,idx+1+C))
-# print(L)
 
+#%%
+C=3
+idx=500
+# print(list(range(0,-1)))
+L=[]
+if idx==0:
+    L=[1,2,3]
+elif idx==1:
+    L=[0,2,3,4]
+elif idx ==2:
+    L=[0,1,3,4,5]
+else:
+    L=list(range(idx-C,idx))+list(range(idx+1,idx+1+C))
+print(L)
 
-# #Tensor,可以接收一组数组作为序号,返回序号对应的元素
-# import torch
+#%%
+#Tensor,可以接收一组数组作为序号,返回序号对应的元素
+import torch
 
-# a=torch.Tensor([1,2,3,4,5,6])
-# print(a[[1,2,3]])
+a=torch.Tensor([1,2,3,4,5,6])
+print(a[[1,2,3]])
 
+#%%
 import torch
 a=torch.Tensor([1,2,3])
 print(a.shape[0])
+
+#%%
+#create data
+x=list(range(0,11))
+y=[i**2 for i in x]
+
+print(x, y)
+
+import torch
+import torch.nn as nn
+import torch.utils.data as tud
+
+class testDataset(tud.Dataset):
+    def __init__(self, x, y):
+        super().__init__()
+        self.x=x
+        self.y=y
+
+    def __len__(self):
+        return len(self.x)
+    
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+
+test=testDataset(x,y)
+print(test[3])
+
+loader=tud.DataLoader(test, batch_size=2, shuffle=True, num_workers=0)
+print(next(iter(loader)))
+print(next(iter(loader)))
+print(next(iter(loader)))
+
+#%%
+import torch
+x = torch.tensor([1, 2, 3, 4])
+print(x,x.shape,x[0])
+b=torch.unsqueeze(x, 0)
+print(b, b.shape,b[0])
+c=torch.unsqueeze(b,2)
+print(c,c.shape)
+
+# %%
+import torch
+x = torch.tensor([[1, 2, 3, 4],
+                  [5, 6, 7, 8],
+                  [9, 10, 11, 12]])
+print(x, x.shape, x[0])
+b = torch.unsqueeze(x, 0)
+print(b, b.shape, b[0],b[0].shape)
+c = torch.unsqueeze(b, 2)
+print(c, c.shape, c[0])
+
+
+#%%
+# https://pytorch.org/docs/stable/generated/torch.squeeze.html
+import torch
+x = torch.tensor([[[1, 2, 3, 4]],
+                  [[5, 6, 7, 8]],
+                  [[9, 10, 11, 12]]])
+print(x, x.shape, x[0])
+b=torch.squeeze(x,0) #leave no change
+print(b,b.shape)
+c=torch.squeeze(x,1)
+print(c,c.shape)
+d=torch.squeeze(x)
+print(d,d.shape)
+
--- 2/wordEmbedding.py
+++ 2/wordEmbedding.py
@@ -170,4 +170,77 @@ dataset=WordEmbeddingDataset(text, word_to_idx, idx_to_word, word_freqs, word_co
 #num_workers: 线程数量
 dataloader=tud.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
 
+#测试
 # next(iter(dataloader))
+
+
+#################################
+#定义PyTorch模型
+#################################
+
+class EmbeddingModel(nn.Module):
+    #定义网络架构所需参数
+    #初始化输入和输出embedding
+    def __init__(self, vocab_size, embed_size):
+        super().__init__()
+
+        self.vocab_size=vocab_size #30000
+        self.embed_size=embed_size #100
+        
+        #定义in和out两个embedding层 in_embed和out_embed相当于参数w
+        #https://pytorch.org/docs/stable/generated/torch.nn.Embedding.html
+        #nn.Embedding通常用于word embedding(https://www.zhihu.com/question/32275069/answer/80188672)
+        #此处输出(30000,100)的embedding
+        self.in_embed=nn.Embedding(self.vocab_size, self.embed_size)
+        self.out_embed=nn.Embedding(self.vocab_size, self.embed_size)
+    
+    #定义forward函数(定义网络架构)
+    #*********************************此处需要进一步分析理解
+    # input_labels: 中心词  (batch_size)个单词
+    # pos_labels: 正确的周围词  (batch_size,(window_size*2))个单词
+    # neg_labels: 负采样中选取的错误的周围词  (batch_size, (window_size*2*K))个单词
+    def forward(self, input_labels, pos_labels, neg_labels):
+
+        #这里进行了运算：（batch_size,vocab_size）*（vocab_size,embed_size）= 128(B) * 100 (embed_size)
+        input_embedding=self.in_embed(input_labels) #(batch_size, embed_size)
+        pos_embedding=self.out_embed(pos_labels) #(batch_size,(window_size*2), embed_size)
+        neg_embedding=self.out_embed(neg_labels) #(batch_size, (window_size*2*K), embed_size)
+
+        #a.unsqueeze(n) 在a的第n维增加一个
+        input_embedding=input_embedding.unsqueeze(2) #(batch_size, embed_size, 1)
+        
+        #计算中心词embedding与周围词embedding的乘积
+        #bmm: If input1 is a (b×n×m) tensor, input2 is a (b×m×p) tensor, out will be a (b×n×p) tensor.
+        #https://pytorch.org/docs/stable/generated/torch.bmm.html
+        #squeeze() 删除维度为1的维度
+        pos_dot=torch.bmm(pos_embedding, input_embedding).squeeze() #(batch_size, (window_size*2))
+        #计算中心词embedding与错误周围词embedding的乘积
+        neg_dot=torch.bmm(neg_embedding, input_embedding).squeeze() #(batch_size, (window_size*2*K))
+        
+        #论文'Distributed Representations of Words and Phrases and their Compositionality'中第3页末尾公式
+        #计算加号前'中心词embedding与周围词embedding的乘积'的logsigmoid
+        log_pos=F.logsigmoid(pos_dot)
+        #计算加号后'中心词embedding与错误周围词embedding的乘积'的logsigmoid
+        log_neg=F.logsigmoid(-neg_dot)
+        #忽略Ewi∼Pn(w)的部分(此操作已在定义neg_words时完成操作)
+        #对加号后面部分log_neg求和
+        log_neg=log_neg.sum(1)
+        #这一步没有出现在公式中...
+        log_pos=log_pos.sum(1)
+
+        loss=log_pos+log_neg
+
+        return -loss
+
+        #取出input_embeddings
+        def input_embeddings(self):
+            return self.in_embed.weight.data.cpu.numpy()
+
+
+#实例化模型
+model=EmbeddingModel(VOCAB_SIZE,EMBEDDING_SIZE)
+
+#使用cuda
+if USE_CUDA:
+    model=model.cuda()
+
